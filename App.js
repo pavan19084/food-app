e8131b1480ef844d247e09e8e9b74ea741ddf537
JayS0223 <jay.s@videosdk.live>
2025-08-14 08:39:18 +0530

    added the reatuart oder screens

--- App.js
+++ App.js
@@ -1,3 +1,46 @@
+// import "react-native-gesture-handler";
+// import React from "react";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createStackNavigator } from "@react-navigation/stack";
+// import Login from "./src/view/auth/login";
+// import Home from "./src/view/home";
+// import SignUp from "./src/view/auth/signup";
+// import "./global.css"
+// import RestaurantScreen from "./src/view/restaurants/RestaurantScreen";
+
+// const Stack = createStackNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="Restaurant">
+//         <Stack.Screen
+//           name="Restaurant"
+//           component={RestaurantScreen}
+//           options={{ headerShown: false }}
+//         />
+//         <Stack.Screen
+//           name="Login"
+//           component={Login}
+//           options={{ headerShown: false }}
+//         />
+//         <Stack.Screen
+//           name="SignUp"
+//           component={SignUp}
+//           options={{ headerShown: false }}
+//         />
+//         <Stack.Screen
+//           name="Home"
+//           component={Home}
+//           options={{ headerShown: false }}
+//         />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+
+
 import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
@@ -5,29 +48,41 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./src/view/auth/login";
 import Home from "./src/view/home";
 import SignUp from "./src/view/auth/signup";
-import "./global.css"
-
-
+import "./global.css";
+import RestaurantScreen from "./src/view/restaurants/RestaurantScreen";
+import CartScreen from "./src/view/restaurants/CartScreen";
 const Stack = createStackNavigator();
 
 export default function App() {
-  return(
+  return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen 
-        name="Login" 
-        component={Login} 
-        options={{headerShown: false}}
+      <Stack.Navigator initialRouteName="Restaurant">
+        <Stack.Screen
+          name="Restaurant"
+          component={RestaurantScreen}
+          options={{ headerShown: false }}
         />
         <Stack.Screen
-        name="SignUp"
-        component={SignUp}
-        options={{headerShown: false}}
+          name="Cart"
+          component={CartScreen}
+          options={{
+            presentation: 'modal',
+          }}
         />
         <Stack.Screen
-        name="Home"
-        component={Home}
-        options={{headerShown: false}}
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
         />
       </Stack.Navigator>
     </NavigationContainer>
